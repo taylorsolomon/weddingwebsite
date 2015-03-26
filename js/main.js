8dc188c882dc85b6c6a4d8e3178876e6b340af2c
Taylor Solomon <taylor.solomon@me.com>
2015-03-25 23:46:12 -0400

    New fonts

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
+jQuery(document).ready(function() {
+  // Hero Parallax
+  var s = skrollr.init({
+    // Don't ensure the viewport has enough room. Keep it tight.
+    forceHeight:false,
+    // No catch-up please
+    smoothScrolling:true,
+    mobile: true,
+  });
+});
+
 (function($) {
+  $(document).ready(function() {
+
+    if ($('#party-teaser').length) {
+      $('#party-teaser').find('.person a').on('click', function(e) {
+        e.preventDefault();
+        var person = $(this).attr('href');
+        console.log($(person).offset().top);
+        $("html, body, .sidy__content").animate({scrollTop: $(person).offset().top}, 500);
+      });
+    }
 
 
-  $(document).ready(function($) {
-    // Hero Parallax
-    var s = skrollr.init({
-      // Don't ensure the viewport has enough room. Keep it tight.
-      forceHeight:false,
-      // No catch-up please
-      smoothScrolling:true,
-      mobile: true,
-    });
   });
 })(jQuery);
-
