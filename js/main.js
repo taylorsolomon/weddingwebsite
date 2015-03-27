365a6aca964253099aed8b46395406cca91d773e
Taylor Solomon <taylor.solomon@me.com>
2015-03-26 22:32:29 -0400

    Scroll fix

--- js/main.js
+++ js/main.js
@@ -1,26 +1,11 @@
-jQuery(document).ready(function() {
-  // Hero Parallax
-  var s = skrollr.init({
-    // Don't ensure the viewport has enough room. Keep it tight.
-    forceHeight:false,
-    // No catch-up please
-    smoothScrolling:true,
-    mobile: true,
-  });
-});
+// jQuery(document).ready(function() {
+//   // Hero Parallax
+//   var s = skrollr.init({
+//     // Don't ensure the viewport has enough room. Keep it tight.
+//     forceHeight:false,
+//     // No catch-up please
+//     smoothScrolling:true,
+//     mobile: true,
+//   });
+// });
 
-(function($) {
-  $(document).ready(function() {
-
-    if ($('#party-teaser').length) {
-      $('#party-teaser').find('.person a').on('click', function(e) {
-        e.preventDefault();
-        var person = $(this).attr('href');
-        console.log($(person).offset().top);
-        $("html, body, .sidy__content").animate({scrollTop: $(person).offset().top}, 500);
-      });
-    }
-
-
-  });
-})(jQuery);
